1fb3101a93fa78eb801a165ca01fd891df66562c
Michael Lynch <mplynch@gmail.com>
2015-08-04 23:17:37 -0500

    Readme change + todo note

--- app/app.js
+++ app/app.js
@@ -8,6 +8,10 @@ angular.module('myApp', [
   'myApp.welcome',
   'myApp.version'
 ]).
+
+// TODO: Replace ng-router with ui-router.  See http://www.funnyant.com/angularjs-ui-router/
+// The bit about state machines is what we need...
+
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
