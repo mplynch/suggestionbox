82619e34c89af50e22a132ed094a169541e0d7b6
Michael Lynch <mplynch@gmail.com>
2015-08-04 22:59:55 -0500

    Application skeleton
    Initial commit of a reasonable start at the application skeleton which
    includes all the needed libraries...

---
+++ app/list/list.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.list', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/list', {
+    templateUrl: 'list/list.html',
+    controller: 'ListCtrl'
+  });
+}])
+
+.controller('ListCtrl', [function() {
+
+}]);
