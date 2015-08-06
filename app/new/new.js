5c17f58def9436d61e7e5622d4a7d93018180666
Michael Lynch <mplynch@gmail.com>
2015-08-06 00:01:41 -0500

    Began creating the new suggestion wizard

--- app/new/new.js
+++ app/new/new.js
@@ -6,18 +6,63 @@ angular.module('myApp.new', ['ui.router'])
 
   $stateProvider
 
-  .state('new', {
+    .state('new', {
     url: '/new',
     templateUrl: 'new/new.html',
     controller: 'NewCtrl'
   })
 
+  .state('new.type', {
+    url: '/new-type',
+    templateUrl: 'new/new-type.html',
+    controller: 'NewCtrl'
+  })
+
   .state('new.actiontype', {
     url: '/actiontype',
-    templateUrl: 'new.actiontype.html'
+    templateUrl: 'new/new-actiontype.html',
+  })
+
+  .state('new.source', {
+    url: '/source',
+    templateUrl: 'new/new-source.html'
+  })
+
+  .state('new.description', {
+    url: '/description',
+    templateUrl: 'new/new-description.html'
   });
 }])
 
-.controller('NewCtrl', [function() {
+.controller('NewCtrl', ['$scope', function($scope) {
+  $scope.types = [{
+    "text": "Health, Safety, Environmental",
+    "value": "HSE"
+  }, {
+    "text": "Quality",
+    "value": "Quality",
+  }, {
+    "text": "Other",
+    "value": "Other"
+  }];
 
+  $scope.actiontypes = [{
+    "text": "Continual Improvement",
+    "value": "Continual Improvement"
+  }, {
+    "text": "Corrective",
+    "value": "Corrective"
+  }, {
+    "text": "Ergonomic",
+    "value": "Ergonomic"
+  }, {
+    "text": "Other",
+    "value": "Other"
+  }, {
+    "text": "Preventative",
+    "value": "Preventative"
+  }, {
+    "text": "Positive Feedback",
+    "value": "Positive Feedback"
+  }];
 }]);
